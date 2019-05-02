142ba558474775a26fd4c7ed9a62697c8214619b
Falco Winkler <falcomail95@yahoo.de>
2019-05-02 20:54:41 +0000

    migrated my own changes to custom and private

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (parinfer cider emmet-mode web-mode markdown-mode haml-mode yaml-mode define-word powerthesaurus flyspell-correct-popup flyspell-correct company shell-pop git-gutter magit counsel-projectile ivy-rich avy flx smex counsel swiper ivy projectile multiple-cursors visual-regexp move-text expand-region which-key vi-tilde-fringe neotree rich-minority smartparens all-the-icons undo-tree simpleclip exec-path-from-shell use-package)))
+    (company-jedi jedi pyenv-mode-auto pyenv-mode parinfer cider emmet-mode web-mode markdown-mode haml-mode yaml-mode define-word powerthesaurus flyspell-correct-popup flyspell-correct company shell-pop git-gutter magit counsel-projectile ivy-rich avy flx smex counsel swiper ivy projectile multiple-cursors visual-regexp move-text expand-region which-key vi-tilde-fringe neotree rich-minority smartparens all-the-icons undo-tree simpleclip exec-path-from-shell use-package)))
  '(shell-pop-shell-type
    (quote
     ("ansi-term" "*ansi-term*"
---
+++ transient/history.el
@@ -0,0 +1,6 @@
+((magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
+ (magit-push nil)
+ (magit-reset nil)
+ (magit-stash nil))
