8dedcc02a34ce7aaab5cf41cdbca2839bceaae72
Falco Winkler <falcomail95@yahoo.de>
2019-05-31 06:14:16 +0000

    started to configure python ide

--- custom.el
+++ custom.el
@@ -5,14 +5,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(elpy-test-runner 'elpy-test-pytest-runner)
  '(package-selected-packages
-   (quote
-    (slack helm ox-twbs htmlize company-jedi jedi pyenv-mode-auto pyenv-mode parinfer cider emmet-mode web-mode markdown-mode haml-mode yaml-mode define-word powerthesaurus flyspell-correct-popup flyspell-correct company shell-pop git-gutter magit counsel-projectile ivy-rich avy flx smex counsel swiper ivy projectile multiple-cursors visual-regexp move-text expand-region which-key vi-tilde-fringe neotree rich-minority smartparens all-the-icons undo-tree simpleclip exec-path-from-shell use-package)))
+   '(realgud terraform-mode slack helm ox-twbs htmlize company-jedi jedi pyenv-mode-auto pyenv-mode parinfer cider emmet-mode web-mode markdown-mode haml-mode yaml-mode define-word powerthesaurus flyspell-correct-popup flyspell-correct company shell-pop git-gutter magit counsel-projectile ivy-rich avy flx smex counsel swiper ivy projectile multiple-cursors visual-regexp move-text expand-region which-key vi-tilde-fringe neotree rich-minority smartparens all-the-icons undo-tree simpleclip exec-path-from-shell use-package))
+ '(realgud:pdb-command-name "py.test --pdb")
  '(shell-pop-shell-type
-   (quote
-    ("ansi-term" "*ansi-term*"
+   '("ansi-term" "*ansi-term*"
      (lambda nil
-       (ansi-term shell-pop-term-shell)))))
+       (ansi-term shell-pop-term-shell))))
  '(shell-pop-universal-key "s-="))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
